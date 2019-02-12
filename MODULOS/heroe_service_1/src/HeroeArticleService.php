3a9f3ca25fbb59698adfbcffa10135724722cade
Rodrigo Igor Villanueva Nieto <rodrigo.villanueva.c@senasica.gob.mx>
2019-02-12 11:50:01 -0600

    Se agrega modulo service 1

---
+++ MODULOS/heroe_service_1/src/HeroeArticleService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\heroe;
+
+/**
+ * Nuestros heroes Articulos class
+ */
+
+class HeroeArticleService{
+
+
+	/**
+	 * Moetodo para caragr los articulos.
+	 */
+	public function getHeroArticles(){
+		$articulo = ['Hulk is green','flash is red'];
+
+		return $articulo;
+	}
+
+}
